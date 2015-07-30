fa0ec1553ac66411db60c44f8d9f8b49e91dd3cc
Lars Lindholm <larlin@lysator.liu.se>
2015-07-30 20:17:59 +0200

    Moved the database connection into memberDB to only expose the controlled db.

---
+++ server/memberDB.js
@@ -0,0 +1,22 @@
+var Cdb = require('./Cdb')
+
+var MongoClient = require('mongodb').MongoClient;
+
+var assert = require('assert')
+var ObjectId = require('mongodb').ObjectID;
+var dbName = "makerMember"
+var dbUrl = 'mongodb://localhost:27017/'+dbName;
+
+var memberDB = function(){};
+
+memberDB.connect = function(callback){
+    MongoClient.connect(dbUrl, function(err, db) {
+        assert.equal(null, err);
+        
+	    var cdb = new Cdb(db);
+	    
+	    callback(null, cdb);
+    });
+};
+
+exports = module.exports = memberDB;
