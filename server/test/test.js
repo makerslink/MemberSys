30d227ab8e0ace397c9b447f8f9e6f8bde3df049
Lars Lindholm <larlin@lysator.liu.se>
2015-08-10 21:58:50 +0200

    Moved test data into two files to have a single member file and a file with more members. Added some tests that verifies that writing to the database actually works.

--- server/test/test.js
+++ server/test/test.js
@@ -13,12 +13,26 @@ var dbName = "makerMember_test"
 var dbUrl = 'mongodb://localhost:27017/'+dbName;
 
 var tempDb;
-var document;
+var multiMembers;
+var singleMember;
+
+//A short and simple test suite for testing that the basic db connection
+// works at all.
 module.exports.bareDb = {
-    readJson: function (test) {
-        fs.readFile('test/testData.json', 'utf8', function (err, data) {
+    readSingleMember: function (test) {
+        test.expect(1);
+        fs.readFile('test/singleMember.json', 'utf8', function (err, data) {
             test.ifError(err);
-            document = JSON.parse(data);
+            singleMember = JSON.parse(data);
+
+            test.done();
+        });
+    },
+    readMultiMembers: function (test) {
+        test.expect(1);
+        fs.readFile('test/multiMembers.json', 'utf8', function (err, data) {
+            test.ifError(err);
+            multiMembers = JSON.parse(data);
 
             test.done();
         });
@@ -40,17 +54,32 @@ module.exports.bareDb = {
             test.done();
         });
     },
-    insert: function (test){
-        test.expect(1);
-        tempDb.collection('members').insert(document, function(err, status) {
+    insertSingle: function (test){
+        test.expect(2);
+        tempDb.collection('members').insert(singleMember, function(err, status) {
             test.ifError(err);
-            tempDb.close();
             
-            test.done();
+            tempDb.collection('members').count("", function(err, result){
+                test.equal(result, 1, "Number of documents not 1 after inserting 1");
+                test.done();
+            });
+        });
+    },
+    insertMulti: function (test){
+        test.expect(2);
+        tempDb.collection('members').insert(multiMembers.members, function(err, status) {
+            test.ifError(err);
+            
+            tempDb.collection('members').count("", function(err, result){
+                // +1 as one member is still in the database from the single member test...
+                test.equal(result, multiMembers.members.length+1, "Number of documents not 1 after inserting 1");
+                test.done();
+                tempDb.close();
+            });
         });
     },
 };
 
-process.on('uncaughtException', function(err) {
-  console.error(err.stack);
-});
+//process.on('uncaughtException', function(err) {
+//  console.error(err.stack);
+//});
