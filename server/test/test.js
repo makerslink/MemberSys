91a8163c8ae96894ac77f78afa8ffe076e6b2d19
Lars Lindholm <larlin@lysator.liu.se>
2015-08-10 21:21:33 +0200

    Switched to a class based variant for testing.

--- server/test/test.js
+++ server/test/test.js
@@ -1,6 +1,7 @@
 "use strict";
 
-var memberDB = require('../memberDB')
+var memberDB = require('../memberDB');
+var testCase = require('nodeunit').testCase;
 
 var MongoClient = require('mongodb').MongoClient;
 var async = require('async')
@@ -10,46 +11,38 @@ var ObjectId = require('mongodb').ObjectID;
 var dbName = "makerMember_test"
 var dbUrl = 'mongodb://localhost:27017/'+dbName;
 
-//Clean database...
-//db.dropCollection(collection, callback);
-//MongoClient.connect(dbUrl, function(err, db)
+var document = '{"fields":[{"type":"email", "value":"test@test.test", "visibility":"private"}]}';
 
-//nodeunit npm nodeunit
-
-
-exports.dbInsert = function(test){
-
-    //TODO: Load this from a json file...
-    var document = '{"fields":[{"type":"email", "value":"test@test.test", "visibility":"private"}]}';
-    var db;
-
-    async.waterfall([
-        function(callback){
-            MongoClient.connect(dbUrl, function(err, tempdb) {
-                test.ifError(err);
-                
-	            db = tempdb
-	            
-	            callback(null);
-            });
-        },
-        function(callback){
-            db.dropDatabase(function(err, status) {
-                test.ifError(err);
-                
-                callback(null);
-            });
-        },
-        function(callback){
-            db.collection('members').insert(JSON.parse(document), function(err, status) {
-                test.ifError(err);
-                
-                db.close();
-                
-                callback(null);
-            });
-        }
-    ]);
-    test.done();
-    
+var tempDb;
+module.exports.bareDb = {
+    connect: function (test) {
+        test.expect(1);
+        MongoClient.connect(dbUrl, function(err, db) {
+            test.ifError(err);
+            tempDb = db;
+            
+            test.done();
+        });
+    },
+    dropDatabase: function (test) {
+        test.expect(1);
+        tempDb.dropDatabase(function(err, status) {
+            test.ifError(err);
+            
+            test.done();
+        });
+    },
+    insert: function (test){
+        test.expect(1);
+        tempDb.collection('members').insert(JSON.parse(document), function(err, status) {
+            test.ifError(err);
+            tempDb.close();
+            
+            test.done();
+        });
+    },
 };
+
+process.on('uncaughtException', function(err) {
+  console.error(err.stack);
+});
